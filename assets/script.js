0e0e323f53e30d2697e0db2de47a36e0ced97a3e
leviathan <jake66martin@hotmail.com>
2023-09-16 18:09:06 -0400

    fetch request code basics implemented

--- assets/script.js
+++ assets/script.js
@@ -5,25 +5,55 @@ var futureWeather = document.querySelector("#fiveDayForecast");
 var button = document.querySelector("#subBtn");
 
 
-// fetch("http://api.openweathermap.org/geo/1.0/direct?q=Ottawa&limit=1&appid=bcc95f0b5af8c8ab3213f526460e43cb")
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data)
-//     })
+button.addEventListener("click", function(event) {
+
+event.preventDefault();
+
+var cityname = cityNameInput.value.trim();
 
 
-button.addEventListener("click", function() {
+if (cityname) {
+    fetchWeather(cityname);
 
-fetch("http://api.openweathermap.org/data/2.5/forecast?lat=45.41&lon=-75.69&appid=bcc95f0b5af8c8ab3213f526460e43cb")
+    cityNameInput.value = ""
+
+}
+
+});
+
+
+
+
+
+var fetchWeather = function(city) {
+
+var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=bcc95f0b5af8c8ab3213f526460e43cb";
+
+fetch(apiUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-        console.log(data)
+      var lon = data[0].lon
+      var lat = data[0].lat
+
+      var weather = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=bcc95f0b5af8c8ab3213f526460e43cb"
+
+      fetch(weather)
+    .then(function (response) {
+      return response.json();
     })
+    .then(function (data) {
+        console.group(data)
+      
+
+    
 })
+
+    console.log(data)
+})};
+
+
 
 
 
